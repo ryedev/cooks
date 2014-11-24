78761dde018d52e40909aa27ac49780760d40370
Joe Tamburro <joetamburro38@gmail.com>
2014-11-23 20:56:47 -0500

    cleaned up scss

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,7 @@
 'use strict';
 
 $(document).ready(function(){
-  $('#tagline').fitText(1.7, { minFontSize: '30px', maxFontSize: '60x' });
-  $('#sub-tag-text').fitText(4, { minFontSize: '14px', maxFontSize: '24px' });
-  $('#main-content-hl').fitText(2, { minFontSize: '16px', maxFontSize: '56px' });
-  $('#main-content-sub-hl').fitText(2, { minFontSize: '10px', maxFontSize: '18px'});
-
-
-  // if(docHeight > 1000) {
-  //   $('.footer-bottom').find('.logo').css({display: 'inline-block'});
-  // }
+  $('#tagline').fitText(1.5, { minFontSize: '30px', maxFontSize: '72x' });
+  $('#sub-tag-text').fitText(4, { minFontSize: '16px', maxFontSize: '24px' });
 });
 
