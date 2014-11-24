cbee7fa67d89cdd194312a98d2a303df9ee8e806
Joe Tamburro <joetamburro38@gmail.com>
2014-11-23 19:35:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Susan Dodds Realtor Website
---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+$(document).ready(function(){
+  $('#tagline').fitText(1.7, { minFontSize: '30px', maxFontSize: '72px' });
+  $('#sub-tag-text').fitText(2, { minFontSize: '11px', maxFontSize: '18px' });
+  $('#main-content-hl').fitText(2, { minFontSize: '16px', maxFontSize: '56px' });
+  $('#main-content-sub-hl').fitText(2, { minFontSize: '10px', maxFontSize: '18px'});
+  
+  // thanks to james padolsey for help with this cross-browswer doc height
+  $.getDocHeight = function(){
+       var D = document;
+       return Math.max(Math.max(D.body.scrollHeight,    D.documentElement.scrollHeight), Math.max(D.body.offsetHeight, D.documentElement.offsetHeight), Math.max(D.body.clientHeight, D.documentElement.clientHeight));
+  };
+  var docHeight = $.getDocHeight();
+
+  // if(docHeight > 1000) {
+  //   $('.footer-bottom').find('.logo').css({display: 'inline-block'});
+  // }
+});
+
