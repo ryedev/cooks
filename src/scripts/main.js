e0e93627440acb2b11e754b3896a589b25cb03a2
Joe Tamburro <joetamburro38@gmail.com>
2014-11-24 11:54:06 -0500

    cleaning up css and what not

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(document).ready(function(){
-  $('#tagline').fitText(1.5, { minFontSize: '30px', maxFontSize: '72x' });
-  $('#sub-tag').fitText(4, { minFontSize: '16px', maxFontSize: '24px' });
+  $('#tagline').fitText(1.5, { minFontSize: '34px', maxFontSize: '72x' });
+  $('#sub-tag').fitText(4, { minFontSize: '15px', maxFontSize: '24px' });
 });
 
