4fd287ff187f5f46a3ddf637cdefb40ec0aca47e
Joe Tamburro <joetamburro38@gmail.com>
2014-11-23 21:32:47 -0500

    cleaning up some styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,6 @@
 
 $(document).ready(function(){
   $('#tagline').fitText(1.5, { minFontSize: '30px', maxFontSize: '72x' });
-  $('#sub-tag-text').fitText(4, { minFontSize: '16px', maxFontSize: '24px' });
+  $('#sub-tag').fitText(4, { minFontSize: '16px', maxFontSize: '24px' });
 });
 
