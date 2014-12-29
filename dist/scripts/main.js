b2beedfff300377d414d36ef8f0b9899080d7a4b
Joe Tamburro <joetamburro38@gmail.com>
2014-12-29 12:09:31 -0500

    Initial dist subtree commit

---
+++ dist/scripts/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(document).ready(function(){
+  $('#tagline').fitText(1, { minFontSize: '46px', maxFontSize: '72x' });
+  $('#sub-tag').fitText(4, { minFontSize: '18px', maxFontSize: '24px' });
+});
+
