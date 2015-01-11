6828269f6e2718154b0c3ab8189afe7726390926
Joe Tamburro <joetamburro38@gmail.com>
2015-01-11 17:03:01 -0500

    reworking hero banner

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(document).ready(function(){
-  $('#tagline').fitText(1, { minFontSize: '46px', maxFontSize: '72x' });
-  $('#sub-tag').fitText(4, { minFontSize: '18px', maxFontSize: '24px' });
+  // $('#tagline').fitText(1, { minFontSize: '46px', maxFontSize: '72x' });
+  // $('#sub-tag').fitText(4, { minFontSize: '12px', maxFontSize: '20px' });
 });
 
