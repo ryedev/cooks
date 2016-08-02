8a8b62ffa6d6febd09f6c39864a68a2448afcbeb
joetamburro <joetamburro38@gmail.com>
2016-08-01 23:57:37 -0400

    added mc modal

--- dist/scripts/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-$(document).ready(function(){
-  // $('#tagline').fitText(1, { minFontSize: '46px', maxFontSize: '72x' });
-  $('#sub-tag').fitText(3, { minFontSize: '14px', maxFontSize: '26px' });
-});
-
