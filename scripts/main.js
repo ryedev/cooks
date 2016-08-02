90ebb822aa4aa08c299f44f32eb1079f9e36a045
joetamburro <joetamburro38@gmail.com>
2016-08-02 00:02:26 -0400

    Initial dist subtree commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(document).ready(function(){
+  // $('#tagline').fitText(1, { minFontSize: '46px', maxFontSize: '72x' });
+  $('#sub-tag').fitText(3, { minFontSize: '14px', maxFontSize: '26px' });
+});
+
